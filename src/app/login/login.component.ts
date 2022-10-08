58cdcc45a03c5e02670459cfc72ed68b92f4d77c
wendy <xiaohui9052@qq.com>
2022-10-09 00:21:21 +0800

    addfile

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+// import { NzInputModule } from 'ng-zorro-antd/input';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+// @NgModule({
+//   imports: [
+//     NzInputModule,
+//     NgZorroAntdModule.forRoot()
+//   ]
+// })
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
